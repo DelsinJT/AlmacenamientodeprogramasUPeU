d1b58505fa6ae376e825f9cd10ee545d0da4004b
DelsinJT <89945769+DelsinJT@users.noreply.github.com>
2021-09-21 09:57:39 -0500

    Add files via upload

---
+++ EstSecuencial/calcularpromediofinal.py
@@ -0,0 +1,13 @@
+print("calcular promedio final de FP")
+#datos de entrada
+nota1=float(input("ingrese nota de evaluacion 1:"))
+nota2=float(input("ingrese nota de evaluacion 2:"))
+nota3=float(input("ingrese nota de evaluacion 3:"))
+nota4=float(input("ingrese nota de evaluacion 4:"))
+nota5=float(input("ingrese nota de evaluacion 5:"))
+nota6=float(input("ingrese nota de evaluacion 6:"))
+nota7=float(input("ingrese nota de evaluacion 7:"))
+#proceso
+profin=(0.15*nota1)+(0.15*nota2)+(0.10*nota3)+(0.15*nota4)+(0.05*nota5)+(0.10*nota6)+(0.30*nota7)
+#datos de salida
+print("el promedio final es:" , profin)
