1ce5eda6902c9ac902b1357cb0ca36a9b999511f
DelsinJT <>
2021-09-21 15:14:57 +0000

    Estcondicional

---
+++ EstSecuencial/EstSecuencial .py
@@ -0,0 +1,8 @@
+#Datos de entrada
+print("Hallar el area del rombo")
+DM=int(input("Ingrese diagonal Mayor:"))
+dm=int(input("Ingerese diagonal Menor"))
+#proceso
+area=(DM*dm)/2
+#Datos de salida
+print("El area del rembo es:", area ,"mt2")
---
+++ Estcondicional/Estcondicional.java
@@ -0,0 +1,16 @@
+class Estcondicional{
+  
+  public static void main(String[] arg){ System.out.println("que ondaaa");
+  int numero1=20;
+  int numero2=30;
+  if(numero1>numero2){
+    System.out.println("numero mayor es:" + numero1); 
+    }else{
+    System.out.println("numero mayor es:" + numero2);
+    }
+  }
+}
+
+  
+  
+    
